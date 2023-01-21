86d440fd3bdbea4d2daf5f0a3a4bb5948b757c57
ruchitha36 <122854121+ruchitha36@users.noreply.github.com>
2023-01-21 19:40:07 +0530

    Add files via upload

---
+++ Comments_Print_Statement.py
@@ -0,0 +1,33 @@
+"""
+Day: Thursday
+Date: 19th Jan 2023
+Topic: Types of comments
+Description: In python there are two types of comments
+1. Single line comment(#)
+2. Multi line comment(''' ''', """ """ )
+"""
+
+""" print statements using comments  """
+
+# print('hey python')
+
+# print('''hey  
+# python''')
+
+# print("""hey  
+# python""")
+
+# print('Hey we are doing python \
+# programming and its \
+# tuesday today we have again \
+# connected back at 8PM')
+
+# print('''Hey we are doing python \
+# programming and its \
+# tuesday today we have again \
+# connected back at 8PM''')
+
+# print('''Hey we are doing python 
+# programming and its
+# tuesday today we have again
+# connected back at 8PM''')
